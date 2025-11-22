f9611ddc43d98f2d54e4671035c2acaa650c3e01
Aman Kumar <amankumar8675@gmail.com>
2025-11-22 10:29:47 +0530

    done

--- app/layout.js
+++ app/layout.js
@@ -38,7 +38,7 @@ export default function RootLayout({ children }) {
 
             <footer className="bg-muted/50 py-12 ">
               <div className="container mx-auto px-4 text-center text-gray-200">
-                <p>made with 💗 by andrewbie</p>
+                <p>made with 💗 by akshat</p>
               </div>
             </footer>
           </ThemeProvider>
